6993df29cdc92709bb64cb9fb511c931180ced9a
Gurkaran Singh <sgurkaran2000@gmail.com>
2020-03-13 06:16:48 +0530

    ..

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 app.listen(2572);
