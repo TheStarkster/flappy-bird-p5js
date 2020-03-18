897733d1265be3fe9ce9fab25cfa9fb4d49ea2dc
Gurkaran Singh <sgurkaran2000@gmail.com>
2020-03-18 21:34:40 +0530

    ..

--- public/sketch.js
+++ public/sketch.js
@@ -17,8 +17,8 @@ function setup() {
   // }
   var canvas = document.getElementById("defaultCanvas0");
   context = canvas.getContext("2d");
-  canvas.width = window.outerWidth * devicePixelRatio;
-  canvas.height = window.outerHeight * devicePixelRatio;
+  canvas.width = window.outerWidth / devicePixelRatio;
+  canvas.height = window.outerHeight / devicePixelRatio;
 }
 
 function draw() {
