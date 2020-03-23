3746fec3e9a446fc74fa69f8f880086fd0af791a
Gurkaran Singh <sgurkaran2000@gmail.com>
2020-03-23 20:47:13 +0530

    axios update

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/response", (req, res) => {
     id: id,
     tid: tid
   };
-  axios.post("http://localhost:5000/tournament/user/score-update", data).then();
+  axios.default.post("http://localhost:5000/tournament/user/score-update",data).then();
 });
 
 app.post("/score", (req, res) => {
